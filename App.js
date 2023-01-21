b3639e31fbca5e6e1860632e3f77cea9c08ba0cf
Faith Umunnakwe <faithuchinonso@gmail.com>
2023-01-21 06:51:57 +0100

    styling

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Getting started!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Write Your Goal" style={styles.inputElement} />
+        <Button title="Add Goal" />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +18,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  inputElement: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    padding: 5,
+    borderRadius: 7,
+    marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
